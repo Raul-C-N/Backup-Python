9938cde10babeae0427820290053ae01723b11fe
Raul C Nadim <raul.nadim@policiacivil.sp.gov.br>
2022-11-24 11:49:40 -0300

    primeira versão funcional

--- Backup Python - function.py
+++ Backup Python - function.py
@@ -10,7 +10,7 @@ from datetime import date
 
 
 # path
-path = 'C:/Users/47889004/Documents/teste'
+path = 'C:/Users/47889004/Documents/BACKUP DTC'
 
 #define a data de hoje
 today = date.today()
@@ -19,6 +19,7 @@ today_str = str(today)
 # Source path
 src = 'W:/ATIVOS DE T.I'
 
+
 #Source list
 src_list = ('W:/ATIVOS DE T.I','W:/SOLICITACOES DE ACESSO REMOTO')
 sl = src_list
@@ -26,39 +27,55 @@ sl = src_list
 
 #destination list end - cria lista com o nome das pastas iteradas da pasta src para ser utilizada para criação do nome de destino
 dest_list_end=[]
+
 for pasta in src_list:
 #    print (pasta)
     print (pasta.strip('W:/'))
     dest_list_end.append(pasta.strip('W:/'))
-dest_list_end 
+#dest_list_end 
 
 # Destination path using today's timestamp
-dest = path+'/'+today_str
+dest_temp = path+'/'+today_str
 
-def Backup(src,dest):
+# Destination list
+dest_list = []
+
+# Cria a lista de destinos final contendo o timestamp e o nome da pasta que foi copiada
+for item in range(len(src_list)):
+    print (dest_temp+'/'+dest_list_end[item])
+    dest_list.append(dest_temp+'/'+dest_list_end[item])
+#dest_list
+
+
+def Backup(src,dest): 
+    # List files and directories
+    # in 'C:/Users / Rajnish / Desktop / GeeksforGeeks'
+    print("Before copying file:")
+    print(os.listdir(path))
+
     # Copy the content of
     # source to destination
-    shutil.copytree(src, dest)
+    destination = shutil.copytree(src, dest)
 
-# Itera pelas pastas da lista de fontes e chama a função de Backup
-for pasta in src_list:
-    print (pasta)
-    #define o destino adicionando dest_end depois do timestamp
+    # List files and directories
+    # in "C:/Users / Rajnish / Desktop / GeeksforGeeks"
+    print("After copying file:")
+    print(os.listdir(path))
+
+    # Print path of newly
+    # created file
+    print("Destination path:", destination)
+
+#Main
+# Itera pelas pastas da lista de fontes (src) e chama a função de Backup para cada uma
+for i in  range(len(src_list)):
+    print (src_list[i]) #fonte
+    print (dest_list[i]) #destino
+    Backup(src_list[i],dest_list[i])
+
+    
     
 
-# List files and directories
-# in 'C:/Users / Rajnish / Desktop / GeeksforGeeks'
-print("Before copying file:")
-print(os.listdir(path))
 
 
 
-
-# List files and directories
-# in "C:/Users / Rajnish / Desktop / GeeksforGeeks"
-print("After copying file:")
-print(os.listdir(path))
-
-# Print path of newly
-# created file
-print("Destination path:", destination)
